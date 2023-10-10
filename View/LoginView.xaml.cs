22e0d9cd61dc9ade7fd0437f0c831fc94f867b2d
Vinicios Macario <JackWyus_01@outlook.com>
2023-10-09 21:01:47 -0300

    Criado Background do LoginView

---
+++ View/LoginView.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UiLoginWithWPF.View
+{
+    /// <summary>
+    /// Lógica interna para LoginView.xaml
+    /// </summary>
+    public partial class LoginView : Window
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //Evento responsável por após segurar com o botão esquerdo do mouse, você pode modificar a posição da janela em relação ao monitor.
+            if(e.LeftButton == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
